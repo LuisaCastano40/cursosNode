0f0e4c318c8df12ba72d7f912ece7521ca7aec84
Luisa Castaño <luisa.castano40@eia.edu.co>
2023-12-13 23:08:34 -0500

    [update] conexión con base de datos 13-12-2023

--- proyectoPrueba/index.js
+++ proyectoPrueba/index.js
@@ -1,15 +1,24 @@
 //importamos dependencias según sistema de módulos ES6
 import express, { Router } from 'express';
+// importamos conexión a base de datos
+import conexionMongo from './config/db.js';
+
+
 //Estoy creando una instancia de express y la asigno a app
 //así se sabe que estamos haciendo un proyecto de node usando express
 const app = express();
+// ejecutamos nuestra función de conexión
+conexionMongo();
 
 // Creamos rutas
 const rutas = express.Router();
 
 //hago la peticion y establezco el endpoint 
 rutas.get('/prueba', (req, res)=>{
-    res.json({ mensaje: "funcionó"});
+    res.json({ mensaje: "funciono bien"});
+})
+rutas.get('/prueba2', (req, res)=>{
+    res.json({ mensaje: "funciono bien x2"});
 })
 
 app.use('/bit', rutas);
